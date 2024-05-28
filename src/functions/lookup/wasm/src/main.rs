b481f9cb785cb372ebce6315bf4d41eaadd7077e
Alejandro Alonso López <aalonsopersonal@gmail.com>
2024-05-28 10:45:59 +0200

    WASM Version - Quick demo

--- src/functions/lookup/wasm/src/main.rs
+++ src/functions/lookup/wasm/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // GET /
+    // let help = warp::get()
+    //     .and(warp::path::end())
+    //     .map(|| "Try POSTing data to /echo such as: `curl localhost:8080/echo -XPOST -d 'hello world'`\n");
+
+    // // POST /echo
+    // let echo = warp::post()
+    //     .and(warp::path("echo"))
+    //     .and(warp::body::bytes())
+    //     .map(|body_bytes: bytes::Bytes| {
+    //         format!("{}\n", std::str::from_utf8(body_bytes.as_ref()).unwrap())
+    //     });
+
+    // let routes = help.or(echo);
+    
+    let welcome = warp::get()
+        .and(warp::path::end())
+        .map(|| "Welcome to the Terminology Service made in Warp");
+
+    let routes = welcome;
+
+    println!("Server started at localhost:8080");
+    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await
 }
