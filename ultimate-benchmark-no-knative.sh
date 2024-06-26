70f45dd36e5691dfb840e2d2b18e95ce25d312db
Alejandro Alonso Lopez <aalonsopersonal@gmail.com>
2024-06-27 00:26:34 +0200

    Reverted comments

--- ultimate-benchmark-no-knative.sh
+++ ultimate-benchmark-no-knative.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 TIME_FILE="curl_time.txt"
-# echo "iteration_number,pod_name,total_time_taken" > startup-times-standard_def.csv
-# echo "iteration_number,pod_name,response_time" > response-time-standard_def.csv
+echo "iteration_number,pod_name,total_time_taken" > startup-times-standard_def.csv
+echo "iteration_number,pod_name,response_time" > response-time-standard_def.csv
 
 
-# for i in {1..200}; do
-#     echo "----------------------------------------------------------------"
-#     echo "Iteration $i"
+for i in {1..200}; do
+    echo "----------------------------------------------------------------"
+    echo "Iteration $i"
 
-#     echo "--- Standard -------------------------------------------------------"
+    echo "--- Standard -------------------------------------------------------"
     
-#     kubectl apply -f src/functions/lookup/standard/kubernetes/000-k8s-deployment.yaml --wait=true --force
+    kubectl apply -f src/functions/lookup/standard/kubernetes/000-k8s-deployment.yaml --wait=true --force
 
-#     sleep 35
+    sleep 35
 
-#     POD_NAME=$(kubectl get pods -l app=lookup-standard -o jsonpath='{.items[0].metadata.name}')
+    POD_NAME=$(kubectl get pods -l app=lookup-standard -o jsonpath='{.items[0].metadata.name}')
 
-#     echo "Storing the standard pod data"
+    echo "Storing the standard pod data"
 
-#     echo "$i,$(journalctl -u k3s.service -o cat | grep -E '*podStartE2EDuration*' | awk '{print $9,$11}' | tail -1 | awk -F'"' '{print $2","$4}')" >> startup-times-standard_def.csv
+    echo "$i,$(journalctl -u k3s.service -o cat | grep -E '*podStartE2EDuration*' | awk '{print $9,$11}' | tail -1 | awk -F'"' '{print $2","$4}')" >> startup-times-standard_def.csv
 
-#     # Port-forward the service to localhost
-#     echo "Port-forwarding service to localhost:8080..."
-#     kubectl port-forward svc/lookup-standard 8080:8080 &
-#     PORT_FORWARD_PID=$!
+    # Port-forward the service to localhost
+    echo "Port-forwarding service to localhost:8080..."
+    kubectl port-forward svc/lookup-standard 8080:8080 &
+    PORT_FORWARD_PID=$!
 
-#     # Wait a few seconds to ensure port-forwarding is established
-#     sleep 5
+    # Wait a few seconds to ensure port-forwarding is established
+    sleep 5
 
-#     # Measure response time
-#     URL="http://localhost:8080"
-#     echo "Measuring response time for $URL"
-#     # { time curl -o /dev/null -s -w "%{http_code}\n" $URL; } 2> $TIME_FILE
+    # Measure response time
+    URL="http://localhost:8080"
+    echo "Measuring response time for $URL"
+    # { time curl -o /dev/null -s -w "%{http_code}\n" $URL; } 2> $TIME_FILE
 
-#     # # Extract the real time from the time command output
-#     # REAL_TIME=$(grep real $TIME_FILE | awk '{print $2}')
+    # # Extract the real time from the time command output
+    # REAL_TIME=$(grep real $TIME_FILE | awk '{print $2}')
     
-#     RESPONSE_TIME=$(curl -o /dev/null -s -w "%{time_total}\n" $URL)
+    RESPONSE_TIME=$(curl -o /dev/null -s -w "%{time_total}\n" $URL)
 
-#     # Kill port-forward process
-#     kill $PORT_FORWARD_PID
+    # Kill port-forward process
+    kill $PORT_FORWARD_PID
 
-#     echo  "$i,$POD_NAME,$RESPONSE_TIME" >> response-time-standard_def.csv 
-#     echo "Deleting standard pod"
+    echo  "$i,$POD_NAME,$RESPONSE_TIME" >> response-time-standard_def.csv 
+    echo "Deleting standard pod"
 
-#     kubectl delete -f src/functions/lookup/standard/kubernetes/000-k8s-deployment.yaml --wait=true --force
+    kubectl delete -f src/functions/lookup/standard/kubernetes/000-k8s-deployment.yaml --wait=true --force
 
-# done
+done
 
 echo "iteration_number,pod_name,total_time_taken" > startup-times-wasm_def.csv
 echo "iteration_number,pod_name,response_time" > response-time-wasm_def.csv
