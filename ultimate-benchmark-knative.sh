be254e9f456c2e33c06eb68887d257a207d6d316
Alejandro Alonso Lopez <aalonsopersonal@gmail.com>
2024-06-27 10:59:15 +0200

    FINISHED TESTING

--- ultimate-benchmark-knative.sh
+++ ultimate-benchmark-knative.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 TIME_FILE="curl_time.txt"
-echo "iteration_number,pod_name,total_time_taken" > startup-times-standard_knative.csv
-echo "iteration_number,pod_name,response_time" > response-time-standard_knative.csv
+# echo "iteration_number,pod_name,total_time_taken" > startup-times-standard_knative.csv
+# echo "iteration_number,pod_name,response_time" > response-time-standard_knative.csv
 
 
-for i in {1..200}; do
-    echo "----------------------------------------------------------------"
-    echo "Iteration $i"
+# for i in {1..200}; do
+#     echo "----------------------------------------------------------------"
+#     echo "Iteration $i"
 
-    echo "--- Standard -------------------------------------------------------"
+#     echo "--- Standard -------------------------------------------------------"
     
-    kubectl apply -f src/functions/lookup/standard/kubernetes/001-knative-service.yaml --wait=true --force
+#     kubectl apply -f src/functions/lookup/standard/kubernetes/001-knative-service.yaml --wait=true --force
 
-    sleep 20
+#     sleep 20
 
-    # Measure response time
-    URL="http://lookup-standard.default.172.28.244.252.sslip.io"
-    echo "Measuring response time for $URL"
+#     # Measure response time
+#     URL="http://lookup-standard.default.172.28.244.252.sslip.io"
+#     echo "Measuring response time for $URL"
     
-    RESPONSE_TIME=$(curl -o /dev/null -s -w "%{time_total}\n" $URL)
+#     RESPONSE_TIME=$(curl --header "Cache-Control: no-cache, no-store" -o /dev/null -s -w "%{time_total}\n" $URL)
 
-    sleep 10
+#     sleep 10
 
-    echo "Storing the standard pod data"
+#     echo "Storing the standard pod data"
 
-    echo "$i,$(journalctl -u k3s.service -o cat | grep -E '*podStartE2EDuration*' | awk '{print $9,$11}' | tail -1 | awk -F'"' '{print $2","$4}')" >> startup-times-standard_knative.csv
+#     echo "$i,$(journalctl -u k3s.service -o cat | grep -E '*podStartE2EDuration*' | awk '{print $9,$11}' | tail -1 | awk -F'"' '{print $2","$4}')" >> startup-times-standard_knative.csv
 
-    POD_NAME=$(journalctl -u k3s.service -o cat | grep -E '*podStartE2EDuration*' | awk '{print $9,$11}' | tail -1 | awk -F'"' '{print $2}')
+#     POD_NAME=$(journalctl -u k3s.service -o cat | grep -E '*podStartE2EDuration*' | awk '{print $9,$11}' | tail -1 | awk -F'"' '{print $2}')
 
-    echo  "$i,$POD_NAME,$RESPONSE_TIME" >> response-time-standard_knative.csv 
-    echo "Deleting standard pod"
+#     echo  "$i,$POD_NAME,$RESPONSE_TIME" >> response-time-standard_knative.csv 
+#     echo "Deleting standard pod"
 
-    kubectl delete -f src/functions/lookup/standard/kubernetes/001-knative-service.yaml --wait=true --force
+#     kubectl delete -f src/functions/lookup/standard/kubernetes/001-knative-service.yaml --wait=true --force
 
-    sleep 20
+#     sleep 20
 
-done
+# done
 
 echo "iteration_number,pod_name,total_time_taken" > startup-times-wasm_knative.csv
 echo "iteration_number,pod_name,response_time" > response-time-wasm_knative.csv
 
-for i in {1..200}; do
+for i in {1..40}; do
     echo "----------------------------------------------------------------"
     echo "Iteration $i"
 
@@ -54,7 +54,7 @@ for i in {1..200}; do
     URL="http://lookup-wasm.default.172.28.244.252.sslip.io"
     echo "Measuring response time for $URL"
     
-    RESPONSE_TIME=$(curl -o /dev/null -s -w "%{time_total}\n" $URL)
+    RESPONSE_TIME=$(curl --header "Cache-Control: no-cache, no-store" -o /dev/null -s -w "%{time_total}\n" $URL)
 
     sleep 10
 
